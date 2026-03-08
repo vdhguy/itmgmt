fb7223c6c8e1f0070582d42f1fa67cd910b0378a
Guy Vander Heyden <guy.vanderheyden@netsap.be>
2026-03-08 16:45:24 +0100

    V1.6

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: require('path').join(__dirname, '.env') });
+require('dotenv').config({ path: require('path').join(__dirname, '.env'), override: true });
 
 const express = require('express');
 const session = require('express-session');
